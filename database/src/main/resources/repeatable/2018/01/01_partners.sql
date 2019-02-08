4491b4ed300726cc04f27bf5ed03a5679dc2b033
Alexey Kaloshin <alexej.kalosin@gmail.com>
2019-02-08 10:37:14 +0100

    lesson resources

---
+++ database/src/main/resources/repeatable/2018/01/01_partners.sql
@@ -0,0 +1,3 @@
+insert into partners values (1,"Bob", "Really nice guy");
+insert into partners values (1,"John", "Good guy");
+insert into partners values (1,"Johnson", "Good guy's friend");
---
+++ integration/src/main/java/eu/learn/Maintenance.java
@@ -0,0 +1,32 @@
+package eu.learn;
+
+/**
+ * @author Alexey Kaloshin
+ */
+public class Maintenance {
+
+	public static void main(String... args) {
+
+		//1
+		getSql();
+
+		//2
+		getXml();
+
+		//3
+		getJson();
+	}
+
+	private static String getJson() {
+		return "";
+	}
+
+	private static String getXml() {
+		return "";
+	}
+
+	private static String getSql() {
+		return "";
+	}
+
+}
