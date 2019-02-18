31305dce434e5d37f92aa17bcd92c143e1f5689c
Alexey Kaloshin <alexej.kalosin@gmail.com>
2019-02-18 13:10:28 +0100

    Connection hierarchy

---
+++ integration/src/main/java/eu/learn/navigation/MockConnection.java
@@ -0,0 +1,10 @@
+package eu.learn.navigation;
+
+/**
+ * @author Alexey Kaloshin
+ */
+public abstract class MockConnection implements Connection {
+	public void connect(String anything) {
+
+	}
+}
---
+++ integration/src/main/java/eu/learn/navigation/OracleConnection.java
@@ -0,0 +1,10 @@
+package eu.learn.navigation;
+
+/**
+ * @author Alexey Kaloshin
+ */
+public abstract class OracleConnection implements Connection {
+	public void connect(String oracleConnectionString) {
+
+	}
+}
